7cfab44803a1a221eb13342a8fb297f17d808b67
sohyeonyun <wja06018@naver.com>
2021-10-19 16:28:42 +0900

    Update: habits.jsx

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,39 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Habit extends Component {
-  state = {
-    count: 0,
-  };
-
   handleIncrement = () => {
-    // state 오브젝트 안에 있는 count를 증가한 뒤 state를 업데이트
-    // this.state.count += 1; 은 리액트가 state가 변경되었는지 모름!!
-    this.setState({ count: this.state.count + 1 });
+    // props으로 전달된 콜백함수 호출, 전달받은 각각의 데이터를 다시 인자로 전해줌.
+    this.props.onIncrement(this.props.habit)
   }
 
   handleDecrement = () => {
-    const count = this.state.count - 1;
-    this.setState({ count: count < 0 ? 0 : count });
+    this.props.onDecrement(this.props.habit)
+  }
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit)
   }
 
   render() {
-    const { name, count } = this.props.habit;
+    const { name, count } = this.props.habit
     return (
       <li className="habit">
         <span className="habit-name">{name}</span>
         <span className="habit-count">{count}</span>
-        <button className="habit-button habit-increase" onClick={this.handleIncrement}>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
           <i className="fas fa-plus-square"></i>
         </button>
-        <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
           <i className="fas fa-minus-square"></i>
         </button>
-        <button className="habit-button habit-delete">
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
           <i className="fas fa-trash"></i>
         </button>
       </li>
-    );
+    )
   }
 }
 
-export default Habit;
+export default Habit
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import Habit from './habit';
+import React, { Component } from 'react'
+import Habit from './habit'
 
 class Habits extends Component {
   state = {
@@ -8,16 +8,29 @@ class Habits extends Component {
       { id: 2, name: 'Running', count: 0 },
       { id: 3, name: 'Coding', count: 0 },
     ],
-  };
+  }
+
+  handleIncrement = habit => {}
+
+  handleDecrement = habit => {}
+
+  handleDelete = habit => {}
+
   render() {
     return (
       <ul>
         {this.state.habits.map(habit => (
-            <Habit key={habit.id} habit={habit}/>
+          <Habit
+            key={habit.id} // props로 데이터 전달해줌.
+            habit={habit}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
         ))}
       </ul>
-    );
+    )
   }
 }
 
-export default Habits;
+export default Habits
