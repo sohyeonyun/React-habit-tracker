95ba29f27a02ca98836b84178ef05b282e44d5af
sohyeonyun <wja06018@naver.com>
2021-10-21 10:59:01 +0900

    Add Form (Ref)

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,64 @@
 import './app.css';
+import React, { Component } from 'react';
 import Habits from './components/habits';
+import Navbar from './components/navbar';
+import HabitAddForm from './components/habitAddForm';
 
-function App() {
-  return <Habits />;
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: 'Reading', count: 0 },
+      { id: 2, name: 'Running', count: 0 },
+      { id: 3, name: 'Coding', count: 0 },
+    ],
+  };
+
+  handleIncrement = habit => {
+    console.log(`handleIncrement ${habit.name}`);
+    const habits = [...this.state.habits]; // spread syntax
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits: habits });
+  };
+
+  handleDecrement = habit => {
+    console.log(`handleDecrement ${habit.name}`);
+    const habits = [...this.state.habits]; // spread syntax
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  };
+
+  handleDelete = habit => {
+    const habits = this.state.habits.filter(item => item.id !== habit.id);
+    this.setState({ habits });
+  };
+
+  handleAdd = name => {
+    const habits = [
+      ...this.state.habits,
+      { id: Date.now(), name: name, count: 0 },
+    ];
+    this.setState({ habits });
+  };
+
+  render() {
+    return (
+      <>
+        <Navbar
+          totalCount={this.state.habits.filter(item => item.count > 0).length}
+        />
+        <Habits
+          habits={this.state.habits}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+          onAdd={this.handleAdd}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,51 +1,27 @@
 import React, { Component } from 'react';
 import Habit from './habit';
+import HabitAddForm from './habitAddForm';
 
 class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: 'Reading', count: 0 },
-      { id: 2, name: 'Running', count: 0 },
-      { id: 3, name: 'Coding', count: 0 },
-    ],
-  };
-
-  handleIncrement = habit => {
-    console.log(`handleIncrement ${habit.name}`);
-    const habits = [...this.state.habits]; // spread syntax
-    const index = habits.indexOf(habit);
-    habits[index].count++;
-    this.setState({ habits: habits });
-  };
-
-  handleDecrement = habit => {
-    console.log(`handleDecrement ${habit.name}`);
-    const habits = [...this.state.habits]; // spread syntax
-    const index = habits.indexOf(habit);
-    const count = habits[index].count - 1;
-    habits[index].count = count < 0 ? 0 : count;
-    this.setState({ habits });
-  };
-
-  handleDelete = habit => {
-    console.log(`handleDelete ${habit.name}`);
-    const habits = this.state.habits.filter(item => item.id !== habit.id);
-    this.setState({ habits });
-  };
-
   render() {
     return (
-      <ul>
-        {this.state.habits.map(habit => (
-          <Habit
-            key={habit.id} // props로 데이터 전달해줌.
-            habit={habit}
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-            onDelete={this.handleDelete}
-          />
-        ))}
-      </ul>
+      <>
+        <HabitAddForm onAdd={this.props.onAdd} />
+        <ul>
+          {this.props.habits.map(habit => (
+            <Habit
+              key={habit.id} // props로 데이터 전달해줌.
+              habit={habit}
+              onIncrement={this.props.onIncrement}
+              onDecrement={this.props.onDecrement}
+              onDelete={this.props.onDelete}
+            />
+          ))}
+        </ul>
+        <button className="habits-reset" onClick={this.props.onReset}>
+          Reset All
+        </button>
+      </>
     );
   }
 }
